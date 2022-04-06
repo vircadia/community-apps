922154504110eb080ba87c8ac90a4ca4f8a001f9
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-04-05 22:13:44 -0400

    Add Places app
    Add Places app

--- applications/metadata.js
+++ applications/metadata.js
@@ -107,6 +107,24 @@ var metadata = { "applications":
             "jsfile": "blocks/Blocks.js",
             "icon": "blocks/blocks-inactive.svg",
             "caption": "BLOCKS"
+        },
+        {
+            "isActive": true,
+            "directory": "odometer",
+            "name": "Odometer",
+            "description": "Tool to record the distance traveled by your avatar.",
+            "jsfile": "odometer/odometer.js",
+            "icon": "odometer/appicon_i.png",
+            "caption": "ODOMETER"
+        },
+        {
+            "isActive": true,
+            "directory": "places",
+            "name": "Places",
+            "description": "Alternative to the 'EXPLORE' application but supporting multiple Metaverse Servers.",
+            "jsfile": "places/places.js",
+            "icon": "places/icons/appicon_i.png",
+            "caption": "ODOMETER"
         }
     ]
 };
